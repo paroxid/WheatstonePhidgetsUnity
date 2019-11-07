cec408a0128e874b7a933ef3fa9924c78e8fb91f
chris <chris@wirmachenbunt.de>
2019-11-07 14:15:15 +0100

    scene and code improved
    debug text to show horizontalWeight and verticalweight

--- Assets/Scripts/PhidgetManager.cs
+++ Assets/Scripts/PhidgetManager.cs
@@ -9,145 +9,97 @@ public class PhidgetManager : MonoBehaviour
 {
 
     [Header("Phidget")]
-    public float multiplier = 150000;
-    public float overallWeight;
-    public Vector4 sensorVals;
+   
     public List<VoltageRatioInput> sensors = new List<VoltageRatioInput>();
 
-    private Vector4 sensorValsTarg, sensorValsCal;
+    float multiplier = 20000;
+    public Vector4 calibratedSensorData, sensorOffset, smoothedSensorData;
     private float dataInt = 16, timer;
-    private bool callibrated;
+    private bool calibrate = true;
 
     [Header("OneEuroFilter Settings")]
-    public bool filterOn = true;
     public float filterFrequency = 120.0f;
     public float filterMinCutoff = 1.0f;
     public float filterBeta = 0.0f;
     public float filterDcutoff = 1.0f;
     OneEuroFilter<Vector4> PhidgetFilter;
 
-    float frontFoot, deltaFrontFoot, backFoot;
+    public float horizontalWeight, verticalWeight;
+
+    void IsAttached(object sender, AttachEventArgs e)
+    {
+        VoltageRatioInput ch = (VoltageRatioInput)sender;
+        sensors[ch.Channel].DataInterval = (int)dataInt;
+    }
+
+    public void DoCalibrate()
+    {
+        if (calibrate)
+        {
+            try
+            {
+                sensorOffset = new Vector4(); //for initial position to level sensor data
+
+                for (int i = 0; i < 4; i++)
+                {
+                    sensors[i].DataInterval = 8;
+                    sensorOffset[i] = (float)(sensors[i].VoltageRatio * multiplier);
+                    print("calibrated");                
+                }
+                
+                calibrate = false;
+            }
+            catch (Exception)
+            {
+                print("not ready yet");
+            }
+        }
+    }
 
     // Start is called before the first frame update
     void Awake()
     {
-        callibrated = true;
         PhidgetFilter = new OneEuroFilter<Vector4>(filterFrequency);
 
         for (int i = 0; i < 4; i++)
         {
             sensors.Add(new VoltageRatioInput());
             sensors[i].Channel = i;
-            sensors[i].VoltageRatioChange += WeightChange;
             sensors[i].Attach += IsAttached;
             sensors[i].Open();
         }
-        Invoke("Callibrate", 0.2f);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        overallWeight = 0;
+        DoCalibrate();
 
-        //OneEuroFilter
-        if (filterOn)
+        if (!calibrate)
         {
             PhidgetFilter.UpdateParams(filterFrequency, filterMinCutoff, filterBeta, filterDcutoff);
-            sensorVals = PhidgetFilter.Filter(sensorValsTarg);
-        }
-        else
-        {
-            sensorVals = sensorValsTarg;
-        }
 
-
-        for (int i = 0; i < 4; i++)
-        {
-            overallWeight += sensorVals[i];
-        }
-
-        if (overallWeight < 1 && !callibrated)
-        {
-            timer += Time.deltaTime;
-            if (timer > 1)
+            for (int i = 0; i < 4; i++)
             {
-                Callibrate();
-            }
-        }
+                // get calibrated sensor values
+                calibratedSensorData[i] = ((float)sensors[i].VoltageRatio * multiplier) - sensorOffset[i];
 
-        if (overallWeight > 2)
-        {
-            callibrated = false;
-        }
+                // smooth the data if needed
+                smoothedSensorData = PhidgetFilter.Filter(calibratedSensorData);
 
-        deltaFrontFoot = (sensorVals[1] + sensorVals[2]) - frontFoot;
-        frontFoot = sensorVals[1] + sensorVals[2];
-        backFoot = sensorVals[0] + sensorVals[3];
-
-
-        //if (GameManager.instance != null)
-        //{
-        //    GameManager.instance.balanceBoardVals = sensorVals;
-        //    GameManager.instance.totalWeight = overallWeight;
-        //    GameManager.instance.frontFoot = frontFoot;
-        //    GameManager.instance.backFoot = backFoot;
-        //    GameManager.instance.deltaFrontFoot = deltaFrontFoot;
-        //    GameManager.instance.moveYBalance = (sensorVals[0] + sensorVals[3]) - (sensorVals[1] + sensorVals[2]);
-        //    GameManager.instance.moveXBalance = (sensorVals[2] + sensorVals[3]) - (sensorVals[0] + sensorVals[1]);
-        //}
-
-    }
-
-    void IsAttached(object sender, AttachEventArgs e)
-    {
-        VoltageRatioInput ch = (VoltageRatioInput)sender;
-        //print("channel attached: "+ch.Channel +"  Data Interval: "+dataInt+" ms");
-        sensors[ch.Channel].DataInterval = (int)dataInt;
-    }
-
-    void WeightChange(object sender, VoltageRatioInputVoltageRatioChangeEventArgs e)
-    {
-        try
-        {
-            VoltageRatioInput axis = (VoltageRatioInput)sender;
-            sensorValsTarg[axis.Channel] = (float)(sensorValsCal[axis.Channel] - sensors[axis.Channel].VoltageRatio * multiplier);
-        }
-        catch (Exception)
-        {
-
-            //
-        }
-        
-    }
-
-    public void Callibrate()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            try
-            {
-                sensorValsCal[i] = (float)(sensors[i].VoltageRatio * multiplier);
-            }
-            catch (Exception)
-            {
-
-               //
+                horizontalWeight = (smoothedSensorData[0] + smoothedSensorData[1] - (smoothedSensorData[2] + smoothedSensorData[3]));
+                verticalWeight = (smoothedSensorData[0] + smoothedSensorData[3]) - (smoothedSensorData[1] + smoothedSensorData[2]);
             }
 
-            
-        }
-        print("callibrated");
-        callibrated = true;
-        timer = 0;
-    }
+            transform.rotation = Quaternion.Euler(new Vector3(horizontalWeight*10, 0, verticalWeight*10));
 
+        }
+    }
 
     private void OnApplicationQuit()
     {
         for (int i = 0; i < 4; i++)
         {
-            sensors[i].VoltageRatioChange -= WeightChange;
             sensors[i].Close();
             sensors[i] = null;
         }
---
+++ Assets/Scripts/SetTextPhidgetDebug.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetTextPhidgetDebug : MonoBehaviour
+{
+    public GameObject PhidgetsManager;
+    public Text mytext;
+
+    public PhidgetManager localPhidgetsmanager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        localPhidgetsmanager = PhidgetsManager.GetComponent<PhidgetManager>(); // get reference of remote script
+        mytext = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mytext.text = localPhidgetsmanager.horizontalWeight.ToString() + "   " + localPhidgetsmanager.verticalWeight.ToString();
+    }
+}
